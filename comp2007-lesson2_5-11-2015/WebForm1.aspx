a28ee38de40e259325ab183c30a42ee0d12b9537
jona2950 <jona2950@gmail.com>
2015-05-11 14:16:57 -0400

    Y

---
+++ comp2007-lesson2_5-11-2015/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="comp2007_lesson2_5_11_2015.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+            <asp:Label ID="lblFirstName" runat="server"></asp:Label>
+            <asp:Literal ID="ltlFirstName" runat="server"></asp:Literal>
+            <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSubmit" runat="server" Text="Click Me" OnClick="btnSubmit_Click" />
+            <asp:Label ID="lblX" runat="server" Visible="false"></asp:Label>
+    </div>
+    <div>
+        <asp:DropDownList runat="server" ID="ddlCountry">
+            <asp:ListItem Value="1" Text="Canada"></asp:ListItem>
+            <asp:ListItem Value="2" Text="United States"></asp:ListItem>
+            <asp:ListItem Value="3" Text="Brazil"></asp:ListItem>
+        </asp:DropDownList>
+
+        <asp:Literal ID="ltlToppings" runat="server"></asp:Literal>
+
+        <asp:CheckBoxList runat="server" ID="cblToppings">
+            <asp:ListItem Value="1" Text="Pepperoni"></asp:ListItem>
+            <asp:ListItem Value="2" Text="Mushrooms"></asp:ListItem>
+            <asp:ListItem Value="3" Text="Green Peppers"></asp:ListItem>
+        </asp:CheckBoxList>
+
+        <asp:Button runat="server" ID="btnSelections" Text="Show Selections" OnClick="btnSelections_Click" />
+            
+    </div>
+    </form>
+</body>
+</html>
---
+++ comp2007-lesson2_5-11-2015/WebForm1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace comp2007_lesson2_5_11_2015
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        public int x = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //add code
+
+            //only increment x if page is NOT being reloaded
+            if (!IsPostBack) { 
+                x++;
+                lblX.Text = x.ToString();
+
+            }
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //get the current count of x from the label 
+            x = Convert.ToInt32(lblX.Text);
+            x++;
+
+            //increment the label value by 1
+            lblX.Text = x.ToString();
+
+            //copy value from textbox to the label
+            lblFirstName.Text = txtFirstName.Text;
+
+            //copy value from textbox to literal
+            ltlFirstName.Text = txtFirstName.Text;
+
+            //remove text from textbox
+            txtFirstName.Text = " ";            
+        }
+
+        protected void btnSelections_Click(object sender, EventArgs e)
+        {
+            //show the selected country in a literal control
+            ltlFirstName.Text = ddlCountry.SelectedValue + " " + ddlCountry.SelectedItem.Text;
+
+            //first clear the literal function for toppings
+            ltlToppings.Text = " ";
+
+            //evaluate checkboxlist by looping through the topping list
+            foreach (ListItem item in cblToppings.Items)
+            {
+                if(item.Selected)
+                {
+                    ltlToppings.Text += item.Text + " ";
+                }
+            }
+        }
+    }
+}
